c6a6bd9ba6b8077656713e920eb80030011c028f
Hilal <hilaltokgoz98@gmail.com>
2022-04-22 11:48:11 +0300

    text control

--- app/src/main/java/com/sorucevap/ResultFragment.kt
+++ app/src/main/java/com/sorucevap/ResultFragment.kt
@@ -1,19 +1,26 @@
 package com.sorucevap
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.sorucevap.databinding.FragmentResultBinding
 import com.zhuinden.fragmentviewbindingdelegatekt.viewBinding
 
 class ResultFragment : Fragment(R.layout.fragment_result) {
-    private  val binding by viewBinding(FragmentResultBinding::bind)
+    private val binding by viewBinding(FragmentResultBinding::bind)
+    private val args: ResultFragmentArgs by navArgs()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        /* bundle ile veri alımı
+        val answer= arguments?.getString("result")
+          binding.resultText.text=answer*/
 
-       val answer= arguments?.getString("result")
-        binding.resultText.text=answer
+        //safeArgs ile veri alımı
+        binding.resultText.text = when{
+            args.result.equals("ankara",true) -> "Tebrikler Bildiniz."
+                else->"Üzgünüz Bilemediniz!"
+        }
     }
 }
