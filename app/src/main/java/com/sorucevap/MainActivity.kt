d3d095743792fb71d38ee5a1c65c45104fd0e98b
Hilal <hilaltokgoz98@gmail.com>
2022-04-23 14:00:06 +0300

    last commit

--- app/src/main/java/com/sorucevap/MainActivity.kt
+++ app/src/main/java/com/sorucevap/MainActivity.kt
@@ -2,11 +2,21 @@ package com.sorucevap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Bottom Bağlama
+        val navHostFragment=
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController= navHostFragment.navController
+        findViewById<BottomNavigationView>(R.id.bottom_nav)
+            .setupWithNavController(navController)
 
     }
 }
